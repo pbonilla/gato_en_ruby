dab79def36809ca398ff64ff3f3859ecd7e333c1
Pablo Bonilla <pabonillavargas@gmail.com>
2013-09-18 11:50:04 -0600

    Adding methods for input and flow control of the game

--- prueba.rb
+++ prueba.rb
@@ -19,7 +19,52 @@ class Game
    puts "----------"
   end
 
-  def insert_position_on_table(x,y,player)
+
+  ##CHECK SECTION##
+  def check_if_there_is_a_winner(player)
+     if(check_rows(player))
+       true
+     elsif(check_columns(player))
+       true
+     #elsif(check_diagonals)
+     ##  true
+     else
+       false
+     end
+  end
+
+  def check_rows(player)
+   result =  @table.map{|row| count_how_many_player_entries_are(player,row)}
+   is_a_winner?(result)
+  end
+
+  def check_columns(player)
+    result = get_columns.map{ |col| count_how_many_player_entries_are(player,col)}
+    is_a_winner?(result)
+  end
+
+  def check_diagonals(player)
+
+  end
+
+  def count_how_many_player_entries_are(player, array)
+    array.select{|entry| entry == player}.count
+  end
+
+  def get_columns
+   columns = Array.new
+   for i in 0..2
+     columns.push([@table[0][i],@table[1][i],@table[2][i]])
+   end
+  end
+  
+  def is_a_winner?(data)
+    data.member?(3)
+  end
+  
+  ## INPUT ##
+  
+ def insert_position_on_table(x,y,player)
     row = @table.slice(x)
     row[y] = player
   end
@@ -30,38 +75,29 @@ class Game
      axle.to_i
   end
 
-  ##CHECK SECTION##
-  def check_if_there_is_a_winner
-     if(check_rows)
-       true
-     elsif(check_columns)
-       true
-     elsif(check_diagonals)
-       true
+
+ ## Main flow ## 
+ def playing_role
+   player = 1
+   while(true)
+     self.show_table
+     print "Player #{player} is your turn\n"
+     x = get_axis_from_keyboard("x")
+     y = get_axis_from_keyboard("y")
+     insert_position_on_table(x,y,player)
+     if(check_if_there_is_a_winner(player))
+        break
+     end
+     if(player == 1)
+       player = 2
      else
-       false
-  end
-
-  def check_rows
-
-  end
-
-  def check_columns
-
-  end
-
-  def check_diagonals
-
-  end
-
-  def hello_world
-    puts "Hello World"
-  end
-  
-   
-end
+       player = 1
+     end
+   end
+ end
+ 
 
 Juego = Game.new
-Juego.show_table
-Juego.insert_position_on_table(Juego.get_axis_from_keyboard("x"),Juego.get_axis_from_keyboard("y"),1)
-Juego.show_table
+Juego.playing_role
+   
+end
